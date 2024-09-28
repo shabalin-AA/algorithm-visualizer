049fdca7e205ec22e9e22de0a485c7ef3d9c1762
Shabalin Aleksei <shabalin0704@outlook.com>
2024-09-28 20:12:53 +0500

    basic rectangle on canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useEffect} from 'react';
+
+function lineRect(ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number) {
+  ctx.strokeRect(x, y, w, h);
+}
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas == null) { return }
+    const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+    if (ctx == null) { return }
+    ctx.fillStyle = 'blue';
+    ctx.lineWidth = 2;
+    lineRect(ctx, 100, 100, 50, 50);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas 
+      id="block-diagram" 
+      width={800} 
+      height={600} 
+      ref={canvasRef} 
+    />
   );
 }
 
