1fb40f8c3918a348adb8cba5f1cda6b331755f34
Shabalin Aleksei <shabalin0704@outlook.com>
2024-10-04 09:55:53 +0500

    react flow template

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import React, {useRef, useEffect} from 'react';
+import Flow from "./Flow";
 
-function lineRect(ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number) {
-  ctx.strokeRect(x, y, w, h);
-}
-
-function App() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas == null) { return }
-    const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
-    if (ctx == null) { return }
-    ctx.fillStyle = 'blue';
-    ctx.lineWidth = 2;
-    lineRect(ctx, 100, 100, 50, 50);
-  }, []);
-
+export default function App() {
   return (
-    <canvas 
-      id="block-diagram" 
-      width={800} 
-      height={600} 
-      ref={canvasRef} 
-    />
+    <div className="App">
+      <Flow />
+    </div>
   );
 }
 
-export default App;
