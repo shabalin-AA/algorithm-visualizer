457293a995d0fb0cf32ad070a05da74bee17779a
Shabalin Aleksei <shabalin0704@outlook.com>
2024-10-12 23:33:10 +0500

    some beauty

--- compiler/src/Main.java
+++ compiler/src/Main.java
@@ -19,33 +19,16 @@ public class Main {
   static Interpreter interpreter;
 
   static HttpServer server;
-  static String root = "/Users/aleksejsabalin/Study/as/algorithm-visualizer/visualizer/build";
+  static String root = System.getProperty("user.dir") + "/../../visualizer/build";
 
   public static void main(String[] args) throws IOException {
-    /*
-    nds = new Node[] {
-      new Node(0, NodeType.CALC, "a = 10; d = 50"),
-      new Node(1, NodeType.COND, "a != 0"),
-      new Node(2, NodeType.CALC, "b = a + 10"),
-      new Node(3, NodeType.CALC, "b = a + 100"),
-      new Node(4, NodeType.CALC, "d")
-    };
-    eds = new Edge[] {
-      new Edge(0, 0, 1),
-      new Edge(1, 1, 2, true),
-      new Edge(2, 1, 3, false),
-      new Edge(3, 2, 4),
-      new Edge(4, 3, 4)
-    };
-    */
-    interpreter = new Interpreter(nds, eds);
-    //i.eval();
+    int port = 3000;
     server = SimpleFileServer.createFileServer(
-      new InetSocketAddress("localhost", 3000), 
+      new InetSocketAddress("localhost", port), 
       Path.of(root), 
       SimpleFileServer.OutputLevel.INFO
     );
-    System.out.println("\n\nStarting server at localhost:3000");
+    System.out.println("\n\nStarting server at localhost:" + port);
     server.start();
   }
 
