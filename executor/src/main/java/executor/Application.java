436e27b32ae4dd160e70049bcf32f4c130063a99
Shabalin Aleksei <shabalin0704@outlook.com>
2024-11-23 22:44:01 +0500

    db

--- executor/src/main/java/executor/Application.java
+++ executor/src/main/java/executor/Application.java
@@ -3,7 +3,9 @@ package executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.*;
 import executor.interpreter.*;
@@ -15,44 +17,49 @@ import java.util.HashMap;
 @SpringBootApplication
 @RestController
 public class Application {
-  SQLiteHandler sqlite = new SQLiteHandler();
+    SQLiteHandler sqlite = new SQLiteHandler();
 
-  @PostMapping(
-    value = "/execute",
-    consumes = "application/json",
-    produces = "application/json"
-  )
-  String execute(@RequestBody String body) {
-    JSONObject jo = new JSONObject(body);
-    System.out.println(jo);
-    JSONArray nds = jo.getJSONArray("Nodes");
-    Node[] nodes = new Node[nds.length()];
-    for (int i = 0; i < nds.length(); i++) {
-      nodes[i] = new Node(nds.getJSONObject(i));
+    @PostMapping(
+        value = "/execute",
+        consumes = "application/json",
+        produces = "application/json"
+    )
+    String execute(@RequestBody String body) {
+        JSONObject jo = new JSONObject(body);
+        System.out.println(jo);
+        JSONArray nds = jo.getJSONArray("Nodes");
+        Node[] nodes = new Node[nds.length()];
+        for (int i = 0; i < nds.length(); i++) {
+            nodes[i] = new Node(nds.getJSONObject(i));
+        }
+        JSONArray eds = jo.getJSONArray("Edges");
+        Edge[] edges = new Edge[eds.length()];
+        for (int i = 0; i < eds.length(); i++) {
+            edges[i] = new Edge(eds.getJSONObject(i));
+        }
+        //TODO: make modules not hardcoded
+        Class<?>[] modules = new Class<?>[] {
+            Math.class
+        };
+        HashMap<Integer, String> results = (new Interpreter(nodes, edges, modules)).eval();
+        JSONObject response = new JSONObject(results);
+        return response.toString();
     }
-    JSONArray eds = jo.getJSONArray("Edges");
-    Edge[] edges = new Edge[eds.length()];
-    for (int i = 0; i < eds.length(); i++) {
-      edges[i] = new Edge(eds.getJSONObject(i));
+
+    @PostMapping(
+        value = "/save",
+        consumes = "application/json"
+    )
+    void save(@RequestBody String body) {
+        sqlite.insertFlowchart(new JSONObject(body));
     }
-    //TODO: make modules not hardcoded
-    Class<?>[] modules = new Class<?>[] {
-      Math.class
-    };
-    HashMap<Integer, String> results = (new Interpreter(nodes, edges, modules)).eval();
-    JSONObject response = new JSONObject(results);
-    return response.toString();
-  }
 
-	@PostMapping(
-    value = "/save",
-    consumes = "application/json"
-  )
-	void save(@RequestBody String body) {
-    sqlite.insertFlowchart(new JSONObject(body));
-	}
+    @GetMapping("/flowchart/{id}")
+    String load(@PathVariable long id) {
+        return sqlite.getFlowchart(id);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- executor/src/main/java/executor/SQLiteHandler.java
+++ executor/src/main/java/executor/SQLiteHandler.java
@@ -11,48 +11,42 @@ import executor.interpreter.Edge;
 
 
 public class SQLiteHandler {
-  private static final String CON_STR = "jdbc:sqlite:../dev.db";
-  private Connection connection;
+    private static final String CON_STR = "jdbc:sqlite:../dev.db";
+    private Connection connection;
 
-  public SQLiteHandler() {
-    try {
-      DriverManager.registerDriver(new JDBC());
-      this.connection = DriverManager.getConnection(CON_STR);
-    } catch (SQLException e) {
-      System.out.println("unable connect to database");
+    public SQLiteHandler() {
+        try {
+            DriverManager.registerDriver(new JDBC());
+            this.connection = DriverManager.getConnection(CON_STR);
+        } catch (SQLException e) {
+            System.out.println("unable connect to database");
+        }
     }
-  }
 
-  void insertNode(Node node, int flowchart_id) {
-    try {
-      Statement st = connection.createStatement();
-      String sql = String.format(
-        "insert into node (node_id, flowchart, code, type) " + 
-                    "values (%d, %d, \"%s\", %d);", 
-        flowchart_id, 
-        node.code, 
-        node.type.ordinal()
-      );
-      st.execute(sql);
-      st.close();
-    } catch (Exception e) {
-      System.out.println("cannot insert node\n" + e.toString());
+    public void insertFlowchart(JSONObject flowchart) {
+        String sql = String.format("insert into flowchart (json) values (\'%s\');", flowchart.toString());
+        System.out.println(sql);
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            stmt.close();
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
     }
-  }
 
-  void insertEdge(Edge edge, int flowchart_id) {
-  }
-
-  public void insertFlowchart(JSONObject flowchart) {
-    JSONArray nds = flowchart.getJSONArray("Nodes");
-    for (int i = 0; i < nds.length(); i++) {
-      Node node = new Node(nds.getJSONObject(i));
-      insertNode(node, 1);
+    public String getFlowchart(long id) {
+        String json = "";
+        String sql = String.format("select json from flowchart where id=%d;", id);
+        System.out.println(sql);
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            json = rs.getString("json");
+            stmt.close();
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
+        return json;
     }
-    JSONArray eds = flowchart.getJSONArray("Edges");
-    for (int i = 0; i < eds.length(); i++) {
-      Edge edge = new Edge(eds.getJSONObject(i));
-      insertEdge(edge, 1);
-    }
-  }
 }
