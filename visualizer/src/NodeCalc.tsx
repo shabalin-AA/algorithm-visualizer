a201b05550bcc927255159905029fda607be9e37
KirillEfanov <kirill.efanov.2003@mail.ru>
2024-11-28 21:22:25 +0500

    add  Output Node

--- visualizer/src/NodeCalc.tsx
+++ visualizer/src/NodeCalc.tsx
@@ -1,35 +1,30 @@
-import { Node, Position, NodeProps, Handle, useReactFlow, NodeResizer} from "@xyflow/react"
-import { memo } from 'react';
+import { Node, Position, NodeProps, Handle, useReactFlow, NodeResizer } from "@xyflow/react"
 import "./styles.css"
 import LimitedConnectionHandle from "./LimitedConnectionHandle"
 
-type Text = Node<{code: string, result: string}>;
+type Text = Node<{code: string, result: string, selected: boolean}>;
 
-
-export default function NodeCalc({id, data}: NodeProps<Text>, selected: boolean | undefined) {
+export default function NodeCalc({id, data, selected}: NodeProps<Text>) {
   const { updateNodeData } = useReactFlow();
   return (
     <div>
-      <div style={{
-        width: "200px",
-        height: "30px",
-        background: '#fff',
-        border: '1px solid #777',
-      }}>
-        <textarea
-         value={data.code}
-         className="node-code"
-         placeholder="c = b + a"
-         onChange={(evt) => updateNodeData(id, {code: evt.target.value})}
-        />
-      </div>
-      <div>
-        <Handle type="target" position={Position.Top}/>
-        <LimitedConnectionHandle type="source" position={Position.Bottom} connectioncount={1}/>
-      </div>
-      <div>
-        {data.result}
-      </div>
+      <body> 
+        <div className="row">
+          <div className="Calc">
+            <textarea
+            value={data.code}
+            className="node-code"
+            placeholder="c = b + a"
+            onChange={(evt) => updateNodeData(id, {code: evt.target.value})}
+            />
+          </div>
+          <div className="Output"><div className="Output-node-code">{data.result}</div></div>
+          <div>
+            <Handle type="target" position={Position.Top} style={{left: 110, top: 8, transform: "translate(-50%, -50%)"}}/>
+            <LimitedConnectionHandle type="source" position={Position.Bottom} style={{left: 110, top: 60, transform: "translate(-50%, -50%)"}} connectioncount={1}/>
+          </div>
+        </div>
+      </body>
     </div>
   );
 };
