8ad6b3c75ab264e4b279d4687f1f3916d965358a
KirillEfanov <kirill.efanov.2003@mail.ru>
2024-11-29 21:00:14 +0500

    add NodeResizer

--- visualizer/src/NodeCalc.tsx
+++ visualizer/src/NodeCalc.tsx
@@ -7,25 +7,23 @@ type Text = Node<{code: string, result: string, selected: boolean}>;
 export default function NodeCalc({id, data, selected}: NodeProps<Text>) {
   const { updateNodeData } = useReactFlow();
   return (
-    <div>
-      <body> 
-        <div className="row">
-          <div className="Calc">
-            <textarea
-            value={data.code}
-            className="node-code"
-            placeholder="c = b + a"
-            onChange={(evt) => updateNodeData(id, {code: evt.target.value})}
-            />
-          </div>
-          <div className="Output"><div className="Output-node-code">{data.result}</div></div>
-          <div>
-            <Handle type="target" position={Position.Top} style={{left: 110, top: 8, transform: "translate(-50%, -50%)"}}/>
-            <LimitedConnectionHandle type="source" position={Position.Bottom} style={{left: 110, top: 60, transform: "translate(-50%, -50%)"}} connectioncount={1}/>
-          </div>
-        </div>
-      </body>
-    </div>
+    <>
+      <NodeResizer 
+      minHeight={50}
+      minWidth={50}
+      isVisible = {selected}
+      />
+        <textarea
+          value={data.code}
+          style={{transform: "rotate(0deg)"}}
+          className="text-input-node__input1 nodrag"
+          placeholder="c = b + a"
+          onChange={(evt) => updateNodeData(id, {code: evt.target.value})}>
+        </textarea>
+        <Handle type="target" position={Position.Top} style={{}}/>
+        <LimitedConnectionHandle type="source" position={Position.Bottom} style={{}} connectioncount={1}/>
+        <div className="text-input-node__input2"> Вывод: {data.result}</div>
+    </>
   );
 };
 
--- visualizer/src/Sidebar.tsx
+++ visualizer/src/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { useDnD } from "./DnDContext";
 
+
 const DnDComponent: FC = () => {
   const [_, setType] = useDnD();
 
@@ -8,21 +9,15 @@ const DnDComponent: FC = () => {
     setType(nodeType);
     event.dataTransfer.effectAllowed = 'move';
   };
-
   return (
     <body>
       <div className="sidebar"> Перетащите узлы, чтобы создать их на поле.
         <div></div> 
-        <div className="dndnode" onDragStart={(event) => onDragStart(event, 'NodeCalc')} draggable>
-          Узел расчета
+        <div onDragStart={(event) => onDragStart(event, 'NodeCalc')} draggable>
+          <div className='dndnode'>Узел расчета</div>
         </div>  
           <div onDragStart={(event) => onDragStart(event, 'NodeIf')} draggable>
-            <svg width="100" height="100" className="shape-svg">
-              <g transform="translate(2, 2)">
-                <path d="M0,48 L48,0 L96,48 L48,96 Z" fill="#ffffff" stroke-width="1" stroke="#000000" fill-opacity="0.8">
-                </path>
-              </g>
-            </svg>
+            <div className='dndnode'>Узел ветвления</div>
           </div>
       </div>
     </body>
